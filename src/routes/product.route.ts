bf898f6c5755034ad26fcf201411e9a5ce11348e
md-abu-kayser <abu.kayser.official@gmail.com>
2026-08-07 00:00:34 +0600

    controller add

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,5 +1,18 @@
 import { IncomingMessage, ServerResponse } from "http";
 
 export const productRoute = (req: IncomingMessage, res: ServerResponse) => {
-  console.log(req.url);
+  //   console.log(req.url);
+  //   console.log(req.method);
+
+  const url = req.url;
+  console.log("Product:", url);
+  const method = req.method;
+
+  if (method === "GET" && url === "/") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "This is root url" }));
+  } else {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "root is not found." }));
+  }
 };
