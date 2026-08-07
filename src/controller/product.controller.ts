f7161f7fc9191200794a5be2e238f0bb9e816630
md-abu-kayser <abu.kayser.official@gmail.com>
2026-08-07 16:59:18 +0600

    chore: service database

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,6 +1,16 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { readProduct } from "../services/product.service";
 
 export const productController = (
   req: IncomingMessage,
   res: ServerResponse,
-) => {};
+) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (method === "GET" && url === "/") {
+    readProduct();
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "This is product route" }));
+  }
+};
