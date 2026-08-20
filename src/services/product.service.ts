632371999788b903fff7e996e6f69fa141d148a4
md-abu-kayser <abu.kayser.official@gmail.com>
2026-08-20 18:15:51 +0600

    feat(database): add JSON file helper functions for product read and write operations

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -5,6 +5,10 @@ const filePath = path.join(process.cwd(), "./src/database/database.json");
 
 export function readProduct() {
   const data = fs.readFileSync(filePath, "utf-8");
+  //   console.log(data.toString());
   return JSON.parse(data);
 }
-readProduct();
+
+export function writeProduct(product: any) {
+  fs.writeFileSync(filePath, JSON.stringify(product));
+}
